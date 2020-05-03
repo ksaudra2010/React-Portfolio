8663bd38319e016c4db05bc648cdd59a958a8448
Audra A Kosinski <57331762+ksaudra2010@users.noreply.github.com>
2020-05-02 19:56:58 -0500

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -11,7 +11,7 @@ function About() {
     <p>Full Stack Web Developer with expertise in both Operations and the Human Side of Business.
         <br/>
         <br/>
-Certificate from University of Kansas Professional Education in full stack web technologies including JavaScript, CSS, HTML, and Node.js. I also maintain a Senior Professional of Human Resources Certification (SPHR) from HRCI and received my MBA from UMKC. Additionally, I am sitting for my Project Management Professional Certification (PMP) exam in May 2020. 
+Certificate from University of Kansas Professional Education in full stack web technologies including JavaScript, CSS, HTML, and Node.js. I also maintain a Senior Professional of Human Resources Certification (SPHR) from HRCI and received my MBA from UMKC. Additionally, I am sitting for my Project Management Professional Certification (PMP) exam in June 2020. 
 <br/> I speak Coding, Operations, and HR, and I can bridge the gap in your projects among subject matter experts. Having mastered everything from program compliance audit creation to automated sorting system troubleshooting, I am the expert you’ve been searching for to achieve and exceed your organization’s project goals. My ability to bring the software and human sides together makes me a valuable addition to your technology team.
     </p>
    <br> </br> 
